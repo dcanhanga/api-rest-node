9bbf2fcebc882b36d97240b4a0a00c95b614991c
Domingos Canhanga <canhanga96@gmail.com>
2024-09-01 22:40:11 +0100

    feat: remove temp database

---
+++ src/@types/knex.d.ts
@@ -0,0 +1,12 @@
+import { Knex } from 'knex';
+declare module 'knex/types/tables' {
+	export interface Tables {
+		transactions: {
+			id: string;
+			sessions_id?: string;
+			title: string;
+			amount: number;
+			created_at?: string;
+		};
+	}
+}
