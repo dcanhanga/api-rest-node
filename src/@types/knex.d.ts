964d541279d32833d71256e9ffede436e1665006
Domingos Canhanga <canhanga96@gmail.com>
2024-09-01 23:02:31 +0100

    feat: List transactions routes

--- src/@types/knex.d.ts
+++ src/@types/knex.d.ts
@@ -1,6 +1,6 @@
 import { Knex } from 'knex';
-declare module 'knex/types/tables' {
-	export interface Tables {
+declare module 'knex/types/tables.js' {
+	interface Tables {
 		transactions: {
 			id: string;
 			sessions_id?: string;
--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -4,6 +4,27 @@ import type { FastifyInstance } from 'fastify';
 import { z } from 'zod';
 
 export async function transactionsRoutes(app: FastifyInstance) {
+	app.get('/', async (_req, rep) => {
+		const transactions = await knex('transactions').select('*');
+		rep.send({
+			transactions,
+		});
+	});
+	app.get('/:id', async (req, rep) => {
+		const getTransactionsParamsSchema = z.object({
+			id: z.string().uuid(),
+		});
+		const { id } = getTransactionsParamsSchema.parse(req.params);
+		const transactions = await knex('transactions')
+			.select('*')
+			.where({
+				id,
+			})
+			.first();
+		rep.send({
+			transactions,
+		});
+	});
 	app.post('/', async (req, rep) => {
 		const createTransactionsBodySchema = z.object({
 			title: z.string(),
