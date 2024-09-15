8955ba3e3f699b1c9715dfc51d474ecf959c6575
Domingos Canhanga <canhanga96@gmail.com>
2024-09-15 17:24:18 +0100

    chore: fix build

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,15 @@ import { env } from './env/index.js';
 app
 	.listen({
 		port: env.PORT,
+		host: env.host,
 	})
 	.then(() => {
 		console.log('HTTP Server Running!');
+		console.log({
+			port: env.PORT,
+			environment: env.NODE_ENV,
+			host: env.host,
+			dataBaseUrl: env.DATA_BASE_URL,
+			dataBaseClient: env.DATA_BASE_CLIENT,
+		});
 	});
